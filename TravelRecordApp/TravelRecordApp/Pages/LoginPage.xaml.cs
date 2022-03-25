03ea81568f1622bf22158b286f170aafad6bcb90
Sadman-Arif-Wamim <sadmanarifwamim11@gmail.com>
2022-03-25 10:27:15 +0600

    Add project files.

---
+++ TravelRecordApp/TravelRecordApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Pages;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
